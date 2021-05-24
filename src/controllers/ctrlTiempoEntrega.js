3e0ee24d3aa298d02a1c4827c5a490cb798ea997
Reimot5 <tomasrubino05@gmail.com>
2021-05-24 16:45:09 -0300

    Ejercicio 6 finalizado

--- src/controllers/ctrlTiempoEntrega.js
+++ src/controllers/ctrlTiempoEntrega.js
@@ -3,8 +3,39 @@ const faker = require('faker')
 module.exports = (() => {
     return {
         calcularTiempoEntrega() {
-            let ranges = require('../utils/ranges.json')
-            return ranges
+            let ranges = [
+                {
+                    range: 1,
+                    distance: 100,
+                    delivery_time: 0
+                },
+                {
+                    range: 2,
+                    distance: 200,
+                    delivery_time: 1
+                }
+            ]
+            let time
+            for (let i = 2; i < 20; i++) {
+                if (ranges.length > 2) {
+                    time = (ranges[i - 1].delivery_time + ranges[i - 2].delivery_time)
+                }
+                ranges[i] = {
+                    range: i + 1,
+                    distance: (ranges[i-1].distance + 100),
+                    delivery_time: time || 1
+                }
+            }
+            console.log(ranges)
+            let distance = faker.datatype.number({ min: 0, max: 2000 })
+            console.log(distance)
+            let index = ranges.findIndex(x => x.distance > distance)
+            console.log(index)
+            let result = {
+                "Distancia a destino del paquete": `${distance} km`,
+                "Tiempo de Entrega": `${ranges[index].delivery_time} dias`,
+            }
+            return result
         },
     }
 })();
